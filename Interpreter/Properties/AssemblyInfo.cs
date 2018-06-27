9ee059acc044ac8e3abfc7d614508bd4a9b155c9
Aliaksandr Tsoi <Aliaksandr.Tsoi@aqr.com>
2018-06-27 14:58:08 -0400

    Migrate Interpreter project to a new csproj format

--- Interpreter/Properties/AssemblyInfo.cs
+++ Interpreter/Properties/AssemblyInfo.cs
@@ -13,12 +13,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Itenso RTF Interpreter")]
-[assembly: AssemblyDescription("Itenso RTF Interpreter")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jani Giannoudis")]
-[assembly: AssemblyProduct("Itenso RTF Interpreter")]
-[assembly: AssemblyCopyright("Copyright © Jani Giannoudis 2004-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
