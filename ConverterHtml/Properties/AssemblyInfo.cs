198c285c97282aa7e6556011b28ae069fc04e412
Aliaksandr Tsoi <Aliaksandr.Tsoi@aqr.com>
2018-06-27 15:20:27 -0400

    Migrate ConverterHtml project to a new csproj format

---
+++ ConverterHtml/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+// name       : AssemblyInfo.cs
+// project    : RTF Framelet
+// created    : Jani Giannoudis - 2008.06.07
+// language   : c#
+// environment: .NET 2.0
+// copyright  : (c) 2004-2013 by Jani Giannoudis, Switzerland
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
---
+++ ConverterHtml/Properties/VersionInfo.cs
@@ -0,0 +1,18 @@
+// name       : VersionInfo.cs
+// project    : RTF Framelet
+// created    : Jani Giannoudis - 2008.05.29
+// language   : c#
+// environment: .NET 2.0
+// copyright  : (c) 2004-2013 by Jani Giannoudis, Switzerland
+
+// ReSharper disable CheckNamespace
+
+namespace Itenso.Rtf.Converter.Html
+// ReSharper restore CheckNamespace
+{
+    public sealed class VersionInfo
+    {
+        /// <value>Provides easy access to the assemblies version as a string.</value>
+        public static readonly string AssemblyVersion = typeof(VersionInfo).Assembly.GetName().Version.ToString();
+    }
+}
